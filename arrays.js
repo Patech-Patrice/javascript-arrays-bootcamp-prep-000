973de51f05e3321de41dd56a2b1e2b01ec04d99a
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-09-13 20:31:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ var chocolatebars = [snickers,hundredgrand,kitkat,skittles];
 function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
 }
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
