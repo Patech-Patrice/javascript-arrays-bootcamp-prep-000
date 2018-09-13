fba1f8c5ac7e6a46a6e47239056dd8ece3ed62f7
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-09-13 20:30:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,8 @@
 var chocolatebars = [snickers,hundredgrand,kitkat,skittles];
 function addElementToBeginningOfArray()
 
+ 
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
+
 function destructivelyAddElementToBeginningOfArray
